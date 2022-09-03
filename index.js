d3defc059ff395a1a985d317ee1c82e321360a51
Pradeep <ynvp48@gmail.com>
2022-09-03 19:16:22 +0530

    Arrow Functions

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-//functions in js
+// arrow functions
+const calcAge = (birthYear) => 2037 - birthYear;
 
-//params
-//function declaration
-function calcAge1(birthYear) {
-    return (2037 - birthYear);
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const ageNow = 2037 - birthYear;
+    const retirement = 65;
+    const yearsLeft = retirement - ageNow;
+    // return yearsLeft;
+    return `${firstName} retires in ${yearsLeft} years`
 }
 
-//args
-const age1 = calcAge1(1999);
-
-console.log(age1);
-
-
-//function without a name
-//after creation assign it to a variable
-// we can call variable with args to invoke the function
-
-// function expression
-
-// function is also considered value in js
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
-
-const age2 = calcAge2(1999)
-console.log(age2)
-
-
-//diff
-// declarations we call before even we define in the code
-// fun expressions, we can't call before we declare them
-
+console.log(yearsUntilRetirement(2020, 'Pradeep'));
