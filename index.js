159987274af915ea8481d575ce1057f74ce5856d
Pradeep <ynvp48@gmail.com>
2022-09-04 15:16:14 +0530

    Arrays basics

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-let calcAverage = (score1, score2, score3) => {
-    return (score1 + score2 + score3) / 3;
-}
+// Arrays
 
-dolphin_average = calcAverage(44, 23, 71);
-koala_average = calcAverage(65, 54, 49);
-console.log(dolphin_average, koala_average);
+const friends = ["Pradeep", "Yendluri", "Naga"];
+console.log(friends);
 
-function checkWinner(avgDolphins, avgKoalas) {
-    if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win 🥇(${avgDolphins} vs. ${avgKoalas})`);
-    }
-    else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win 🎉(${avgKoalas} vs. ${avgDolphins})`);
-    } else {
-        console.log('No team won the match');
-    }
-}
+const years = new Array(1991, 1994, 1200);
+console.log(years);
 
-checkWinner(dolphin_average, koala_average);
-checkWinner(1000, 11100);
+const classes = new Array('B123', 'B213', 23)
+console.log(classes);
+
+console.log(friends.length);
+console.log(years.length)
+console.log(classes.length);
+
+//last element
+console.log(friends[friends.length - 1])
+
+console.log(friends[0]);
+console.log(classes[4]);
+
+// can mutate element of an array with diff element
+// cannot change entire array
+
+friends[2] = 'Vara';
+console.log(friends)
+
+const firstName = 'Yendluri'
+const pradeep = [firstName, 'Naga', "Vara", 2037 - 1991, 'student', friends];
+
+console.log(pradeep.length)
