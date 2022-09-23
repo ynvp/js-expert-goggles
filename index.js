1d40ddadbee92729a88d0da4c5a4ebdafcef27f6
Pradeep <ynvp48@gmail.com>
2022-09-24 02:57:14 +0530

    js-final exercise

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
-let miller = {
-    fullName: 'Mark Miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height * this.height);
-        return this.bmi;
-    },
-
-    summary: function (compName, compBMI) {
-        console.log(`${this.fullName}'s BMI (${this.bmi} is higher than ${compName}'s (${compBMI}))`)
-    }
+let calcTip = (bill) => {
+    console.log(bill);
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-let smith = {
-    fullName: 'John Smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height * this.height);
-        return this.bmi;
-    },
+bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+tips = [];
+totals = [];
 
-    summary: (compName, compBMI) => {
-        console.log(`${this.fullName}'s BMI (${this.bmi}) is higher than ${compName}'s (${compBMI})`)
+for (let bill in bills) {
+    let tip = calcTip(bills[bill]);
+    console.log(tip);
+    tips.push(tip);
+    totals.push(bills[bill] + tip);
+}
+
+console.log(tips, totals);
+
+let calcAverage = (arr) => {
+    let sum = 0;
+    for (let i in arr) {
+        sum += arr[i];
     }
-}
+    return sum / arr.length;
+};
 
-smith.calcBMI();
-miller.calcBMI();
+console.log(calcAverage(tips));
 
-if (smith.bmi > miller.bmi) {
-    smith.summary(miller.fullName, miller.bmi)
-} else {
-    console.log('x')
-    miller.summary(smith.fullName, smith.bmi)
-}
+const calcAge = (birthYear) => birthYear - 30;
+
+console.log("hello1");
